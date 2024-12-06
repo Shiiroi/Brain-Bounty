8e7e340f3dff73698bad3e75b097d6aee41d8904
Vince Magwili <vmagwili@gmail.com>
2024-12-06 15:02:02 +0000

    main menu

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-name = input("Enter Player Name: ")
+from scripts import game
 
-print(name)
+# name = input("Enter Player Name: ")
+
+# print(name)
+
+def mainMenu():
+    print("\nWelcome to Brain Bounty!")
+    print("[1] Play Game")
+    print("[2] Highscores")
+    print("[0] Exit")
+    return int(input("\nEnter a Number: "))
+
+def main():
+    while True:
+        choice = mainMenu()
+
+        if choice == 1:
+            game.play("pokemon")
+        elif choice == 2:
+            ...
+        elif choice == 0:
+            ...
+
+main()
---
+++ scripts/game.py
@@ -0,0 +1,3 @@
+def play(category):
+    print(f"Assigned task {category}")
+    return int(input("Select Difficulty of Enemy"))
